f76b93311c2cc4c0df8eea95a693d558b0bc7da5
eunjungleo <asaprocky123@likelion.org>
2020-07-20 04:51:32 +0900

    touch models and serializers

--- django_backend/ani/models.py
+++ django_backend/ani/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Content(models.Model):
+    title = models.TextField() # load directly from youtube API
+    url_youtube = models.URLField()
+    description = models.TextField()
+    origins = models.CharField(max_length=10) # country
+
+    def __str__(self):
+        return print(self.title + self.origin)
+    
+    
